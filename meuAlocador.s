eedc7092a6a43659621881ea4208a5a7f9afdc6c
Henrique Rieger <henriquerieger2001@gmail.com>
2022-04-21 03:40:32 -0300

    [fix] comenta e corrige liberaMem

--- meuAlocador.s
+++ meuAlocador.s
@@ -84,123 +84,129 @@ liberaMem:
     movq %rax, -24(%rbp)  # topoAtualHeap <- sbrk(0)
 
     # se bloco < topoInicialHeap retorna 0
-    movq -16(%rbp), %rax 
-    cmpq %rax, TOPO_INICIAL_HEAP
-    jge retorno_0
-
-    # se bloco >= topoAtualHeap retorna 0
-    cmpq %rax, -24(%rbp)
+    movq -16(%rbp), %rax            # %rax <- bloco
+    cmpq TOPO_INICIAL_HEAP, %rax    # %rax < topoInicialHeap
     jl retorno_0
 
+    # se bloco >= topoAtualHeap retorna 0
+    cmpq -24(%rbp), %rax    # %rax >= topoAtualHeap
+    jge retorno_0
+
     # se bloco[-16] == 0 retorna 0
-    cmpq $0, -16(%rax)
+    cmpq $0, -16(%rax)  # bloco[-16] == 0
     je retorno_0
 
     movq $0, -16(%rax)  # bloco[-16] <- 0
 
     # primBloco <- topoInicialHeap+16
-    movq TOPO_INICIAL_HEAP, %rax
-    addq $16, %rax
-    movq %rax, -32(%rbp)
+    movq TOPO_INICIAL_HEAP, %rax    # %rax <- topoInicialHeap
+    addq $16, %rax                  # %rax <- topoInicialHeap + 16
+    movq %rax, -32(%rbp)            # primBloco <- %rax
 
-# while(primBloco[-16] == 1) primBloco += primBloco[-8] + 16 
+# while(primBloco[-16] == 1) 
 comeco_loop_prim_bloco:
-    movq -32(%rbp), %rax
-    movq $1, %rbx
-    cmpq -16(%rax), %rbx
+    movq -32(%rbp), %rax        # %rax <- primBloco
+    movq $1, %rbx               # %rbx <- 1
+    cmpq -16(%rax), %rbx        # 1 == primBloco[-16]
     jne fim_loop_prim_bloco
-    addq -8(%rax), %rax
-    addq $16, %rax
-    movq %rax, -32(%rbp)
-    jmp comeco_loop_prim_bloco
-fim_loop_prim_bloco:
-    movq %rax, -32(%rbp)
 
+    # primBloco += primBloco[-8] + 16
+    addq -8(%rax), %rax         # %rax <- primBloco + primBloco[-8]
+    addq $16, %rax              # %rax <- primBloco + primBloco[-8] + 16
+    movq %rax, -32(%rbp)        # primBloco <- %rax
+    jmp comeco_loop_prim_bloco
+
+fim_loop_prim_bloco:
     # tamBloco <- primBloco[-8]
-    movq -8(%rax), %rax
-    movq %rax, -40(%rbp)
+    movq -32(%rbp), %rax    # %rax <- primBloco
+    movq -8(%rax), %rax     # %rax <- %rax[-8]
+    movq %rax, -40(%rbp)    # tamBloco <- %rax
 
     # proxBloco <- primBloco + tamBloco + 16
-    movq -32(%rbp), %rax
-    addq -40(%rbp), %rax
-    addq $16, %rax
-    movq %rax, -48(%rbp)
+    movq -32(%rbp), %rax    # %rax <- primBloco
+    addq -40(%rbp), %rax    # %rax <- primBloco + tamBloco
+    addq $16, %rax          # %rax <- primBloco + tamBloco + 16
+    movq %rax, -48(%rbp)    # proxBloco <- %rax
 
 # while(proxBloco < topoAtualHeap)
 comeco_loop_grande_libera:
-    movq -48(%rbp), %rax
-    cmpq %rax, -24(%rbp)
+    movq -48(%rbp), %rax        # %rax <- proxBloco
+    cmpq -24(%rbp), %rax        # proxBloco < topoAtualHeap
     jge fim_loop_grande_libera
 
-    # #if(bloco[-16] == 0)
-    movq $0, %rbx
-    cmpq -16(%rax), %rbx
-    jne else_loop_grande_libera
+    # #if(proxBloco[-16] == 0)
+    movq -48(%rbp), %rax        # %rax <- proxBloco
+    movq $0, %rbx               # %rbx <- 0
+    cmpq -16(%rax), %rbx        # 0 == proxBloco[-16]
+    jne else_loop_grande_libera 
 
     # tamBloco += proxBloco[-8] + 16
-    movq -40(%rbp), %rbx
-    addq -8(%rax), %rbx
-    addq $16, %rbx
-    movq %rbx, -40(%rbp)
+    movq -48(%rbp), %rax    # %rax <- proxBloco
+    movq -40(%rbp), %rbx    # %rbx <- tamBloco 
+    addq -8(%rax), %rbx     # %rbx <- %proxBloc[-8]
+    addq $16, %rbx          # %rbx <- %proxBloc[-8] + 16
+    movq %rbx, -40(%rbp)    # tamBloco <- %rbx
 
     # proxBloco += proxBloco[-8] + 16
-    addq -8(%rax), %rax
-    addq $16, %rax
-    movq %rax, -48(%rbp)
+    movq -48(%rbp), %rax    # %rax <- proxBloco
+    addq -8(%rax), %rax     # %rax <- proxBloco + proxBloco[-8]
+    addq $16, %rax          # %rax <- proxBloco + proxBloco[-8] + 16
+    movq %rax, -48(%rbp)    # proxBloco <- %rax
 
     jmp fim_if_loop_grande_libera
 
 else_loop_grande_libera:
     # primBloco[-8] <- tamBloco
-    movq -32(%rbp), %rax
-    movq -40(%rbp), %rbx
-    movq %rbx, -8(%rax)
+    movq -32(%rbp), %rax    # %rax <- primBloco
+    movq -40(%rbp), %rbx    # rbx <- tamBloco
+    movq %rbx, -8(%rax)     # primBloco[-8] <- %rbx
 
 # while(proxBloco < topoAtualHeap && proxBloco[-16] == 1)
 comeco_loop_pequeno_libera:
-    movq -48(%rbp), %rax
-    movq -24(%rbp), %rbx
-    cmpq %rax, %rbx
-    jge fim_loop_pequeno_libera
-    movq $1, %rbx
-    cmpq -16(%rax), %rbx
+    movq -48(%rbp), %rax        # %rax <- proxBloco
+    movq -24(%rbp), %rbx        # %rbx <- topoAtualHeap
+    cmpq %rbx, %rax             # proxBloco < topoAtualHeap
+    jge fim_loop_pequeno_libera 
+    movq $1, %rbx               # %rbx <- 1
+    cmpq -16(%rax), %rbx        # 1 == proxBloco[-16]
     jne fim_loop_pequeno_libera
 
     # proxBloco += proxBloco[-8] + 16
-    addq -8(%rax), %rax
-    addq $16, %rax
-    movq -48(%rbp), %rax
+    movq -48(%rbp), %rax    # %rax <- proxBloco
+    addq -8(%rax), %rax     # %rax <- proxBloco + proxBloco[-8]
+    addq $16, %rax          # %rax <- proxBloco + proxBloco[-8] + 16
+    movq %rax, -48(%rbp)    # proxBloco <- %rax
 
     jmp comeco_loop_pequeno_libera
 fim_loop_pequeno_libera:
     # primBloco <- proxBloco
-    movq %rax, -48(%rbp)
-    movq -32(%rbp), %rax
+    movq -48(%rbp), %rax    # %rax <- proxBloco
+    movq %rax, -32(%rbp)    # primBloco <- %rax
 
     # tamBloco <- primBloco[-8]
-    movq %rax, -32(%rbp)
-    subq $8, %rax
-    movq 0(%rax), %rax
-    movq %rax, -40(%rbp)
+    movq -32(%rbp), %rax    # %rax <- primBloco
+    movq -8(%rax), %rax     # %rax <- primBloco[-8]
+    movq %rax, -40(%rbp)    # tamBloco <- %rax
 
     # proxBloco <- primBloco+tamBloco+16
-    movq -32(%rbp), %rax
-    addq -40(%rbp), %rax
-    addq $16, %rax
-    movq -48(%rbp), %rax
+    movq -32(%rbp), %rax    # %rax <- primBloco
+    addq -40(%rbp), %rax    # %rax <- primBloco + tamBloco
+    addq $16, %rax          # %rax <- primBloco + tamBloco + 16
+    movq %rax, -48(%rbp)    # proxBloco <- %rax
 
 fim_if_loop_grande_libera:
     jmp comeco_loop_grande_libera
+
 fim_loop_grande_libera:
     # #if(tamBloco > primBloco[-8])
-    movq -40(%rbp), %rax
-    movq -32(%rbp), %rbx
-    cmpq %rax, -8(%rbx)
+    movq -40(%rbp), %rax    # %rax <- tamBloco
+    movq -32(%rbp), %rbx    # %rbx <- primBloco
+    cmpq -8(%rbx), %rax     # tamBloco > primBloco[-8]
     jle retorno_1
 
     # primBloco[-8] <- tamBloco
-    movq -32(%rbp), %rbx
-    movq %rax, -8(%rbx)
+    movq -32(%rbp), %rbx    # %rbx <- primBloco
+    movq %rax, -8(%rbx)     # primBloco[-8] <- tamBloco
 
 retorno_1:
     movq $1, %rax
